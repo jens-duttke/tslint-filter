ac7b75e2681cf7afd459a58f425b1545353bb68b
Jens Duttke <j.duttke@modix.eu>
2019-01-17 16:28:16 +0100

    Extend "___import-name" rule to return the whole import source

--- test/rules/___importNameRule.js
+++ test/rules/___importNameRule.js
@@ -1,1 +1,13 @@
-module.exports = require('../../dist')('tslint-microsoft-contrib/importNameRule');
+const Utils = require('tsutils');
+
+module.exports = require('../../dist')('tslint-microsoft-contrib/importNameRule', {
+	suppressErrors: true,
+	modifyFailure (failure) {
+		if (/^Misnamed import\./.test(failure.failure)) {
+			const node = Utils.getTokenAtPosition(failure.sourceFile, failure.getStartPosition().getPosition());
+
+			failure.failure += ` for '${node.parent.moduleSpecifier.text}'`
+		}
+		return failure;
+	}
+});
