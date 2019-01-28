11cace513ca85fefee04c50c1fd12d081bd8e340
Jens Duttke <github@duttke.de>
2019-01-28 21:54:29 +0100

    Remove deprecated "suppressErrors" setting

--- rules/___importNameRule.js
+++ rules/___importNameRule.js
@@ -1,7 +1,6 @@
 const Utils = require('tsutils');
 
 module.exports = require('../dist')('tslint-microsoft-contrib/importNameRule', {
-	suppressErrors: true,
 	modifyFailure (failure) {
 		if (/^Misnamed import\./.test(failure.failure)) {
 			const node = Utils.getTokenAtPosition(failure.sourceFile, failure.getStartPosition().getPosition());
