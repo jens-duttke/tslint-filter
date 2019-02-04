fd81e3f8d09ad95da8c8d334e3ead8df9c8d2de2
Jens Duttke <github@duttke.de>
2019-02-04 19:52:47 +0100

    Remove modifyFailure method

--- rules/___typedefRule.js
+++ rules/___typedefRule.js
@@ -1,16 +1,1 @@
-const utils = require('tsutils');
-
-module.exports = require('../dist')('tslint/lib/rules/typedefRule', {
-	modifyFailure (failure) {
-		// Don't require return type definition of arrow functions, if it's assigned to a variable, which must also have a type definition
-		if (failure.failure === 'expected arrow-call-signature to have a typedef' && this.ruleArguments.includes('variable-declaration')) {
-			const node = utils.getTokenAtPosition(failure.sourceFile, failure.getStartPosition().getPosition());
-
-			if (utils.isVariableDeclaration(node.parent.parent)) {
-				return;
-			}
-		}
-
-		return failure;
-	}
-});
+module.exports = require('../dist')('tslint/lib/rules/typedefRule');
