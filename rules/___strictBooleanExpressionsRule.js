bcaa6ce0fbb2259d69f9d5e91e64974e9f538a58
Jens Duttke <github@duttke.de>
2019-02-04 19:52:17 +0100

    Add comment

--- rules/___strictBooleanExpressionsRule.js
+++ rules/___strictBooleanExpressionsRule.js
@@ -5,9 +5,10 @@ module.exports = require('../dist')('tslint/lib/rules/strictBooleanExpressionsRu
 	modifyFailure (failure) {
 		let node = utils.getTokenAtPosition(failure.sourceFile, failure.getStartPosition().getPosition()).parent;
 
+		// Find nearest parent which is not an expression to determine the context
 		while (utils.isExpression(node) && !utils.isJsxExpression(node) && (node = node.parent) !== undefined) {}
 
-		failure.failure = failure.failure.replace(/ operator because /, ` operator in ${ts.SyntaxKind[node.kind]} because `)
+		failure.failure = failure.failure.replace(/ operator because /, ` operator in ${ts.SyntaxKind[node.kind]} because `);
 
 		return failure;
 	}
