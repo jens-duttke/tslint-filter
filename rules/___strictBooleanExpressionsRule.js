5853d00a21b5d907a3241120f8762586786ec2c3
Jens Duttke <github@duttke.de>
2019-02-04 16:44:27 +0100

    Extend the error message to show the usage-context

--- rules/___strictBooleanExpressionsRule.js
+++ rules/___strictBooleanExpressionsRule.js
@@ -1,1 +1,14 @@
-module.exports = require('../dist')('tslint/lib/rules/strictBooleanExpressionsRule');
+const ts = require('typescript');
+const utils = require('tsutils');
+
+module.exports = require('../dist')('tslint/lib/rules/strictBooleanExpressionsRule', {
+	modifyFailure (failure) {
+		let node = utils.getTokenAtPosition(failure.sourceFile, failure.getStartPosition().getPosition()).parent;
+
+		while (utils.isExpression(node) && !utils.isJsxExpression(node) && (node = node.parent) !== undefined) {}
+
+		failure.failure = failure.failure.replace(/ operator because /, ` operator in ${ts.SyntaxKind[node.kind]} because `)
+
+		return failure;
+	}
+});
